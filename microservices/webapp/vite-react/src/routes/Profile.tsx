22a21c06e591311133f0195586da9b1e99f26595
Dominic Pham <dom17pham@gmail.com>
2023-04-19 02:41:42 +0000

    Created routes to other services from homepage and made changes to homepage layout

--- microservices/webapp/vite-react/src/routes/Profile.tsx
+++ microservices/webapp/vite-react/src/routes/Profile.tsx
@@ -1,8 +1,13 @@
+import { Avatar, Center } from "@mantine/core"
+
 export function Profile() {
 
   return (
     <div>
       <h1>My Profile</h1>
+      <Center inline>
+        <Avatar radius="xl" size="xl"/>
+      </Center>
     </div>
   )
 }
