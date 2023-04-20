fef2b016b322cad9ae65fd7dc75d95bbea6ad883
Dominic Pham <dom17pham@gmail.com>
2023-04-20 03:04:40 +0000

    Missing files Added.

---
+++ microservices/webapp/vite-react/src/routes/BookingRoom.tsx
@@ -0,0 +1,7 @@
+export function BookingRoom(){
+    return (
+        <div>
+          <h1>Book a Room</h1>
+        </div>
+      )
+}
---
+++ microservices/webapp/vite-react/src/routes/Groups.tsx
@@ -0,0 +1,10 @@
+import {GroupTable} from "../components/Groups/Table";
+export function Groups(){
+    
+    return (
+        <div>
+          <h1>My Groups</h1>
+          <GroupTable/>
+        </div>
+      )
+}
